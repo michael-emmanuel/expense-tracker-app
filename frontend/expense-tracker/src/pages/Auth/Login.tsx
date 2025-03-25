89c23f3d06186db9608a2c7ad1d9c690ee53ef47
Michael Emmanuel <michael.emmanuel732@gmail.com>
2025-03-25 18:39:53 -0400

    set up frotend routes

---
+++ frontend/expense-tracker/src/pages/Auth/Login.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div>
+      <div className=''>
+        <h3 className=''>Welcome Back</h3>
+        <p className=''>Please enter your details to log in</p>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
